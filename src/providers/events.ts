551634fd50c049010af93dea9d3781dc20e141fd
Caetano <lucas.caetanov2013@gmail.com>
2023-09-28 11:33:17 -0300

    auth

--- src/providers/events.ts
+++ src/providers/events.ts
@@ -1,5 +1,5 @@
 
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Injectable, OnDestroy, OnInit} from "@angular/core";
 import {environment} from "../environments/environment.prod";
 
@@ -8,6 +8,13 @@ export class EventsProvider {
     constructor(public http: HttpClient,) {
     }
     apiEvent = '/atividades'
+    headers
+    ngOnInit() {
+       this.headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.get('token')}`
+      });
+    }
     getEvents(){
         return new Promise((resolve, reject) => {
             console.log(environment.api + this.apiEvent)
@@ -40,7 +47,7 @@ export class EventsProvider {
   deleteEvent(id){
     return new Promise((resolve, reject) => {
       console.log(environment.api + this.apiEvent)
-      this.http.delete(environment.api +   this.apiEvent+ "/destroy/"+ id)
+      this.http.delete(environment.api +   this.apiEvent+ "/destroy/"+ id,this.headers)
         .subscribe((result) => {
           console.log(result)
             resolve(result);
@@ -54,7 +61,7 @@ export class EventsProvider {
   postEvent(body){
     return new Promise((resolve, reject) => {
       console.log(environment.api, body )
-      this.http.post(environment.api + this.apiEvent+'/store', body)
+      this.http.post(environment.api + this.apiEvent+'/store', body,this.headers)
         .subscribe((result) => {
           console.log(result)
             resolve(result);
@@ -68,7 +75,7 @@ export class EventsProvider {
   putEvent(body,id){
     return new Promise((resolve, reject) => {
       console.log(environment.api, body )
-      this.http.post(environment.api + this.apiEvent+'/update/'+id, body)
+      this.http.post(environment.api + this.apiEvent+'/update/'+id, body,this.headers)
         .subscribe((result) => {
           console.log(result)
             resolve(result);
